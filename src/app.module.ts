24098d2a53260cd14461135b2a887993eacd633f
RaoofJM <62191843+RaoofJM@users.noreply.github.com>
2023-09-04 18:34:16 +0330

    throttler (rate limiter) added

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { TaskModule } from './task/task.module';
+import { ThrottlerModule } from '@nestjs/throttler';
+import { throttlerConfig } from './config/throttler.config';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost/task-manager'),
+    ThrottlerModule.forRoot(throttlerConfig),
     TaskModule,
   ],
   controllers: [AppController],
